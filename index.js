78935d79dad6f3a37abf679b51238ac92103f6ec
Anthony Grimes <agrimes@charter.net>
2017-09-25 18:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function endGame() {
     alert("YOU LOSE!");
     // update the "START" label
     var again = document.getElementById('start');
-    again.innerHTML = 'PLAY AGAIN'?
+    //again.innerHTML = 'PLAY AGAIN'?
     //again.style.display = 'block';
 }
 
