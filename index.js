915b21fe0c111fe11ff7060eb816ec913be68e46
Anthony Grimes <agrimes@charter.net>
2017-09-25 18:51:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,8 +128,6 @@ function createRock(x) {
 function endGame() {
     clearInterval(gameInterval);
     window.removeEventListener('keydown', moveDodger);
-    console.log(ROCKS);
-    ROCKS.length = 0;
     var elems = document.getElementsByClassName('rock');
     while (elems[0]) {
         elems[0].parentNode.removeChild(elems[0]);
